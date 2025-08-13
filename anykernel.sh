52e8bba443ee81b483291cdeaf68d3bcf8b8268a
xradens <boydeaths007@gmail.com>
2025-08-13 07:44:48 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Watermelon Kernel by xradens
+kernel.string=Roters Khatulistiwa by xradens
 do.devicecheck=1
 do.modules=0
 do.systemless=1
